aa9f63deb960058b8974463180bd5b7e29117b81
ASUS <Ayda_21$a>
2024-01-24 23:47:11 +0330

    Add Ammo and mag + Add Health + Reload Animation

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+   public float walkspeed = 3f;
 
+    [Space]
+
+    private Vector2 input;
+
+    private Rigidbody rb;
 
     public float health =100f;
 
+// Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+       
+    }
+
+    
     public void TakeDamage(float damage){
 
         health -= damage;
